4cdb9bb7287a34c6d4cb19bc1dc3ee37d7f8aacd
brandon-yan <hanchong1128@sjtu.edu.cn>
2020-07-09 15:38:39 +0800

    git is hard

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 
 
 int main() {
-    freopen("C:\\Users\\hanchong\\Desktop\\RISC-V\\riscv-testcases\\testcases\\bulgarian.data", "r", stdin);
-    freopen("C:\\Users\\hanchong\\Desktop\\RISC-V\\riscv-testcases\\testcases\\answer.txt", "w", stdout);
+    //freopen("C:\\Users\\hanchong\\Desktop\\RISC-V\\riscv-testcases\\testcases\\bulgarian.data", "r", stdin);
+    //freopen("C:\\Users\\hanchong\\Desktop\\RISC-V\\riscv-testcases\\testcases\\answer.txt", "w", stdout);
     int pos = 0, tmp;
     std::string buffer;
     while (std::getline(std::cin, buffer)) {
