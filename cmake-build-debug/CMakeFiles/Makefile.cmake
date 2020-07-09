1ccda9ea579ce42206b5e49f5ea5d1ccca532307
brandon-yan <hanchong1128@sjtu.edu.cn>
2020-07-09 15:13:47 +0800

    first version

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -53,5 +53,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/RISC_V_simulator.dir/DependInfo.cmake"
+  "CMakeFiles/code.dir/DependInfo.cmake"
   )
